f3de5b4f782106a84f7cbdf49f65a086b68b3fb5
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-07-28 14:55:24 -0600

    create constructor for individual tasks (FullTask)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-var taskName = '';
-var taskDate = new Date();
-var fullTask = {
-    taskName: taskName,
-    taskDate: taskDate,
-};
+"use strict";
+class FullTask {
+    constructor(n, d, e) {
+        this.taskName = n;
+        this.taskDate = d;
+        this.taskDescription = e;
+    }
+    format() {
+        return `${this.taskName} should be completed by ${this.taskDate}: ${this.taskDescription}`;
+    }
+}
+const newTask = new FullTask('Test', new Date('2023-08-01'), 'Test');
+let tasks = [];
+function addTask() {
+    let taskName = document.getElementById('inputTask').value;
+    let taskDate = new Date(document.getElementById('dueDate').value);
+    let taskDescription = document.getElementById('inputDescription').value;
+    const newTask = new FullTask(taskName, taskDate, taskDescription);
+    tasks.push(newTask);
+    tasks.forEach(task => {
+        console.log(task.format());
+    });
+}
--- scripts.ts
+++ scripts.ts
@@ -1,7 +1,32 @@
-const taskName: string = '';
-const taskDate: Date = new Date();
+class FullTask {
+  taskName: string;
+  taskDate: Date;
+  taskDescription: string;
 
-let fullTask = {
-  taskName,
-  taskDate,
-};
+  constructor(n: string, d: Date, e: string){
+    this.taskName = n;
+    this.taskDate = d;
+    this.taskDescription = e;
+  }
+
+  format() {
+    return `${this.taskName} should be completed by ${this.taskDate}: ${this.taskDescription}`
+  }
+}
+
+const newTask = new FullTask('Test', new Date('2023-08-01'), 'Test');
+
+let tasks: FullTask[] = [];
+
+function addTask() {
+  let taskName = (<HTMLInputElement>document.getElementById('inputTask')).value;
+  let taskDate = new Date((<HTMLInputElement>document.getElementById('dueDate')).value);
+  let taskDescription = (<HTMLInputElement>document.getElementById('inputDescription')).value;
+  const newTask = new FullTask(taskName, taskDate, taskDescription);
+  tasks.push(newTask);
+
+  tasks.forEach(task => {
+  console.log(task.format());
+});
+}
+
