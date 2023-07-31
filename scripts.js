af37d3e590be216d1b99ff54cb7423874705e60a
Chris F <93630113+gonna-boop@users.noreply.github.com>
2023-07-31 09:54:50 -0600

    create use of modules, update addtask function

--- scripts.js
+++ scripts.js
@@ -1,14 +1,23 @@
-import FullTask from './classes/tasks.js';
-const newTask = new FullTask('Test', new Date('2023-08-01'), 'Test');
-let tasks = [];
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tasks_js_1 = require("./classes/tasks.js");
+// let docOne: hasFormatter;
+// let docTwo: hasFormatter;
+// docOne = new FullTask('Test Task', new Date('2023-08-01'), 'Test Task Description');
+// docTwo = new CompleteTask('Test Task 2', new Date('2023-08-02'), 'Test Task 2 Description');
+// let tasks: hasFormatter[] = [];
+// tasks.push(docOne);
+// tasks.push(docTwo);
+// console.log(tasks);
 function addTask() {
-    let taskName = document.getElementById('inputTask').value;
-    let taskDate = new Date(document.getElementById('dueDate').value);
-    let taskDescription = document.getElementById('inputDescription').value;
-    const newTask = new FullTask(taskName, taskDate, taskDescription);
-    tasks.push(newTask);
-    tasks.forEach(task => {
-        console.log(task.format());
-    });
+    var taskComplete = false;
+    var taskName = document.getElementById('inputTask').value;
+    var taskDate = new Date(document.getElementById('dueDate').value);
+    var taskDescription = document.getElementById('inputDescription').value;
+    var newTask = new tasks_js_1.default(taskName, taskDate, taskDescription);
+    // tasks.push(newTask);
+    // tasks.forEach(task => {
+    // console.log(task.format());
 }
+;
 document.getElementById("addBtn").addEventListener("click", addTask);
--- scripts.ts
+++ scripts.ts
@@ -1,19 +1,30 @@
 import  FullTask from './classes/tasks.js';
+import  CompleteTask from './classes/completeTasks.js';
+import  { hasFormatter } from './interfaces/hasformatter.js'
 
-const newTask = new FullTask('Test', new Date('2023-08-01'), 'Test');
+// let docOne: hasFormatter;
+// let docTwo: hasFormatter;
 
-let tasks: FullTask[] = [];
+// docOne = new FullTask('Test Task', new Date('2023-08-01'), 'Test Task Description');
+// docTwo = new CompleteTask('Test Task 2', new Date('2023-08-02'), 'Test Task 2 Description');
+
+// let tasks: hasFormatter[] = [];
+// tasks.push(docOne);
+// tasks.push(docTwo);
+
+// console.log(tasks);
 
 function addTask() {
+  let taskComplete = false;
   let taskName = (<HTMLInputElement>document.getElementById('inputTask')).value;
   let taskDate = new Date((<HTMLInputElement>document.getElementById('dueDate')).value);
   let taskDescription = (<HTMLInputElement>document.getElementById('inputDescription')).value;
   const newTask = new FullTask(taskName, taskDate, taskDescription);
-  tasks.push(newTask);
+  // tasks.push(newTask);
 
-  tasks.forEach(task => {
-  console.log(task.format());
-});
-}
+  // tasks.forEach(task => {
+  // console.log(task.format());
+};
+ 
 
 document.getElementById("addBtn")!.addEventListener("click", addTask);
